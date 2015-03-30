356de638e1d74c29a63bdbcbd0696f8f3f554cba
Thinh Hung <hungnt.dev@gmail.com>
2015-03-30 17:06:17 +0700

    first commit

---
+++ html/src/com/yuri/drop/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.yuri.drop.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.yuri.drop.Drop;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Drop();
+        }
+}
